baa0693634e23e177738f4874596b6d84c2ecf13
Federico Ramirez <74111018+Federico492@users.noreply.github.com>
2024-09-30 18:52:27 +0100

    Functions.kt

---
+++ main.kt
@@ -0,0 +1,70 @@
+//STORE A FUNCTION IN A VARIABLE
+
+//To store a function into another function 
+fun main(){
+  val trickFunction = ::trick
+}
+
+fun trick(){
+  println("No treats!")
+}
+
+//To store a funciton into another function but with a lambda expression
+
+fun main(){
+  val trickFunction = trick
+  trick()
+}
+val trick = { //instead of fun will be val
+  println("No treats!")
+}
+
+//Use function types and lambda expressions in Kotlin
+
+fun main(){
+  val trickFunction = trick
+  trick()
+}
+val trick = { //instead of fun will be val
+  println("No treats!")
+}
+
+val treat: () -> Unit = {
+  println("Have a treat!")
+}
+
+//////////////////////////////////
+
+fun main() {
+    val coins: (Int) -> String = {
+        "$it quarters"
+    }
+
+    val cupcake: (Int) -> String = {
+        "Have a cupcake!"
+    }
+
+    val treatFunction = trickOrTreat(false, coins)
+    val trickFunction = trickOrTreat(true, cupcake)
+    treatFunction()
+    trickFunction()
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?) : () -> Unit{
+  if (isTrick){
+    return trick
+  } else {
+    if(extraTreat != null) {
+      println(extraTreat(5))
+    }
+    return treat
+  }
+}
+
+val trick = { 
+  println("No treats!")
+}
+
+val treat: () -> Unit = {
+  println("Have a treat!")
+}
